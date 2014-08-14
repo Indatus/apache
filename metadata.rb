35014f43b55dd5162bac369ee02796e65513171e
savamane <savamane@gmail.com>
2014-08-14 20:23:16 +0300

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 maintainer       'Brian Webb'
 maintainer_email 'bwebb@indatus.com'
 description       "Configures apache"
-version           "1.0"
+version           "1.1"
 supports          'ubuntu'
 depends           "build-essential"
